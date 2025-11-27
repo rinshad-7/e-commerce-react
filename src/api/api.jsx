62685f27a39990da7e90c419f6c254da6ad94f1f
rinshad <rinshadmelethalakkal123@gmail.com>
2025-11-27 14:13:03 +0530

    api updation

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/", 
+  baseURL: "http://13.61.105.87:3000", 
   withCredentials:true,
 });
 
